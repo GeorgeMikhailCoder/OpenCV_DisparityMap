841a20b1cadbbcaa04e5577c306f9b58b9b3b7a6
GeorgeMikhailCoder <george.mikhail@yandex.ru>
2022-02-19 11:47:55 +0300

    init

---
+++ OpenCV_DisparityMap/OpenCV_DisparityMap.cpp
@@ -0,0 +1,13 @@
+﻿// OpenCV_DisparityMap.cpp: определяет точку входа для приложения.
+//
+
+#include "OpenCV_DisparityMap.h"
+#include<opencv2/opencv.hpp>
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
